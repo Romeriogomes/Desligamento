957211eefbf032d93fdf7241db7ce3107161ec19
Romeriogomes <103548264+Romeriogomes@users.noreply.github.com>
2022-09-01 17:17:29 -0300

    nova versão

---
+++ codigos.js
@@ -0,0 +1,11 @@
+
+
+
+function texto(){
+ salario = document.getElementById('sal').innerHTML;
+  dataentrada = document.getElementById('adm').innerHTML
+  document.getElementById('text').innerHTML = "ok";  
+}
+function display(){
+     document.getElementById('result').innerHTML = sral;
+}
